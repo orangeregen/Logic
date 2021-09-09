2ddd574078d6e1856aeea554a24626dfc8539b9a
Alexandra <83174783+orangeregen@users.noreply.github.com>
2021-09-09 21:24:56 +0300

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <locale.h>
+#include <ctime>
+
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int i;
+	int arr[10] = {};
+	srand(time(NULL));
+	for (i = 0; i < 10; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+	printf("Массив: ");
+	for (i = 0; i < 10; i++)
+		printf("%d ", arr[i]);
+}
